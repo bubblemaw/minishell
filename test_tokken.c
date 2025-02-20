363d5521892c8fc2c060d1f98fc5975750429d3c
David Chellen <davidchellen07@gmail.com>
2025-02-20 17:14:20 +0100

    faire encore la gestion des quotes

--- test_tokken.c
+++ test_tokken.c
@@ -6,7 +6,7 @@
 /*   By: david <david@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 16:02:21 by dchellen          #+#    #+#             */
-/*   Updated: 2025/02/19 17:28:19 by david            ###   ########.fr       */
+/*   Updated: 2025/02/20 17:13:21 by david            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,29 @@
 
 int	skip_space(char *str, int *i)
 {
+	int check_first;
+
+	check_first = *i;
 	while ((str[*i] >= 9 && str[*i] <= 13) || str[*i] == 32)
 		(*i)++;
+	if (check_first == 0 && (str[*i] == '<'
+		|| str[*i] == '>' || str[*i] == '|'))
+		return (ERROR);
 	return (0);
 }
 
 int	double_quotes(char *str, int *i)
 {
+	// int before;
+	// int after;
+
 	if (str[*i] == '"')
 	{
+		// before = *i - 1;
+		// if (before != 0 && (str[before] != ' ' && str[before] != '>'
+		// 	&& str[before] != '<' && str[before] != '|'
+		// 	&& str[before] != '='))
+		// 	return (ERROR);
 		(*i)++;
 		while (str[*i] != '"')
 			(*i)++;
@@ -124,6 +138,19 @@ void	add_node(t_shell *shell, t_chain *new)
 	return ;
 }
 
+// void free_list(t_chain *head)
+// {
+// 	t_chain *temp;
+
+// 	while (head != NULL)
+// 	{
+// 		temp = head;
+// 		head = head->next;
+// 		free(temp);
+// 	}
+// 	return ;
+// }
+
 void	print_chain(t_chain *head)
 {
 	t_chain	*current;
@@ -145,32 +172,42 @@ int	creat_tokken(char *input, t_shell *shell)
 	int		begin;
 	int		end;
 	char	*content;
+	char	*last_token;
 	t_chain	*new;
-	
+
 	i = 0;
 	end = 0;
 	while (input[i] != '\0')
 	{
-		skip_space(input, &i);
+		if (skip_space(input, &i) == ERROR)
+			return (ERROR);
 		begin = i;
 		if (double_quotes(input, &i) == VALID)
 			end = i;
+		else if (double_quotes(input, &i) == ERROR)
+			return (ERROR);
 		else if (single_quotes(input, &i) == VALID)
 			end = i;
 		else if (detect_redirections(input, &i) == ERROR)
 			return (ERROR);
 		else if (detect_redirections(input, &i) == VALID)
+		{
 			end = i + 1;
+			i++;
+		}
 		else if (detect_command(input, &i) == VALID)
 			end = i;
 		else if (detect_command(input, &i) == ERROR)
 			return (ERROR);
 		content = ft_substr(input, begin, end - begin);
+		printf("tokken : %s\n", content);
 		new = creat_node(content);
 		add_node(shell, new);
-		printf("tokken : %s\n", content);
-		i++;
 	}
+	last_token = content;
+	if (last_token[0] == '<' || last_token[0] == '>'
+		|| last_token[0] == '|')
+		return (ERROR);
 	printf("\n---------------------\n");
 	print_chain(shell->tokken);
 	return (0);
@@ -184,6 +221,8 @@ int	main (void)
 	ft_memset(&shell, 0, sizeof(t_shell));
 	while (1)
 	{
+		// if (shell.tokken != NULL)
+			// free_list(shell.tokken);
 		input = readline("minishell$ ");
 		if (strncmp(input, "exit ", 4) == 0)
 		{
--- to_do.md
+++ to_do.md
@@ -1,7 +1,12 @@
+
+
 # A FAIRE
 
 **tokkenisation**
 
-- gestion erreur des quotes
-- dernier token en trop dans certains cas
+- des quotes doivent toujours etre suivie d un
+  espace ou d'une redirection
+- les erreur doivent tout kill sans rien exectuer
+    - dans mon cas si une redirection est fausse
+      alors je dois afficher un message d'erreur et rien d autre
 - free la liste chainee pour eviter l ajout
