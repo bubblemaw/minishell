e8846390e948ed50125e97ff8c3a93a22ba30630
David Chellen <davidchellen07@gmail.com>
2025-03-27 17:41:11 +0100

    gerer les doubles

--- srcs/builtin/built_in.c
+++ srcs/builtin/built_in.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   built_in.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dchellen <dchellen@student.42.fr>          +#+  +:+       +#+        */
+/*   By: david <david@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/26 16:34:33 by maw               #+#    #+#             */
-/*   Updated: 2025/03/26 11:22:12 by dchellen         ###   ########.fr       */
+/*   Updated: 2025/03/27 17:22:27 by david            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,15 +21,15 @@ int	built_in(t_cmd *cmd, t_shell *shell)
 		export(cmd, shell);
 		return (VALID);
 	}
-	else if(ft_strlen(cmd->arg[0]) >= 2 && ft_strncmp(cmd->arg[0], "cd", 2) == 0)
+	else if(ft_strlen(cmd->arg[0]) == 2 && ft_strncmp(cmd->arg[0], "cd", 2) == 0)
 		return (cd(cmd, shell));
-	else if(ft_strlen(cmd->arg[0]) >= 3 && ft_strncmp(cmd->arg[0], "env", 3) == 0)
+	else if(ft_strlen(cmd->arg[0]) == 3 && ft_strncmp(cmd->arg[0], "env", 3) == 0)
 		return (ft_env(cmd, shell));
-	else if(ft_strlen(cmd->arg[0]) >= 3 && ft_strncmp(cmd->arg[0], "pwd", 3) == 0)
+	else if(ft_strlen(cmd->arg[0]) == 3 && ft_strncmp(cmd->arg[0], "pwd", 3) == 0)
 		return(pwd());
-	else if(ft_strlen(cmd->arg[0]) >= 5 && ft_strncmp(cmd->arg[0], "unset", 5) == 0)
+	else if(ft_strlen(cmd->arg[0]) == 5 && ft_strncmp(cmd->arg[0], "unset", 5) == 0)
 		return(unset(cmd, shell));
-	else if(ft_strlen(cmd->arg[0]) >= 4 && ft_strncmp(cmd->arg[0], "exit", 4) == 0)
+	else if(ft_strlen(cmd->arg[0]) == 4 && ft_strncmp(cmd->arg[0], "exit", 4) == 0)
 	{
 		ft_exit(cmd, shell);
 		return (VALID);
